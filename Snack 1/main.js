d66be006ac6190d282279f913b953eb9c4b6281b
FabioMorrone <morronefabio04@gmail.com>
2025-02-21 15:09:42 +0100

    first commit

---
+++ Snack 1/main.js
@@ -0,0 +1,23 @@
+/* 
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+
+// Creare un array di oggetti
+
+const bici = [ 
+    {
+        nome : bianchi,
+        peso : 35,
+    },
+    {
+        nome : cannondale,
+        peso : 43,
+    },
+    {
+        nome : santaCruz,
+        peso : 21,
+    }
+]
+
