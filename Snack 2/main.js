2206ba88da3dfdff72c1378112ec7c4644ee2f7c
FabioMorrone <morronefabio04@gmail.com>
2025-02-21 16:26:39 +0100

    add 2 esercizio

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -5,3 +5,23 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+
+const squadre = [
+    {
+        'nome' : "Milan", 
+        'punti_fatti' : 0, 
+        'falli_subiti' : 0, 
+    }, 
+    {
+        'nome' : "Barcellona", 
+        'punti_fatti' : 0, 
+        'falli_subiti' : 0, 
+    }, 
+    {
+        'nome' : "Cagliari", 
+        'punti_fatti' : 0, 
+        'falli_subiti' : 0, 
+    }
+]
+
